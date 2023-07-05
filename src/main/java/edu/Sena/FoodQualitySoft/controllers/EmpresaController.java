fef276e99aaa8679e6bfab7e1387d2ebf9ecfa21
Saul Echeverri <secheverri@cidenet.com.co>
2023-07-04 21:24:03 -0500

    Implementando mensajes popups en empresa controller

--- src/main/java/edu/Sena/FoodQualitySoft/controllers/EmpresaController.java
+++ src/main/java/edu/Sena/FoodQualitySoft/controllers/EmpresaController.java
@@ -25,25 +25,29 @@ public class EmpresaController {
 
 
     @GetMapping("VerEmpresas")
-    public String viewEmpresas(Model model) {
+    public String viewEmpresas(Model model, @ModelAttribute("mensaje")String mensaje) {
+        //Recibo el parametro @ModelAttribute... porque me va aresibir el mensaje implementado en los otros servicios
         // Obtener la lista de empresas utilizando el método getAllEmpresas() del servicio empresaService
         List<Empresa> empresaList = empresaService.getAllEmpresas();
 
-        // Agregar la lista de empresas al modelo de la vista con el nombre "empList"
+        // Agregar la lista de empresas al modelo de la vista con el nombre "empList" y el de "mensaje"
         model.addAttribute("empList", empresaList);
+        model.addAttribute("mensaje", mensaje);
 
         // Devolver el nombre de la vista "verEmpresas" que se mostrará al cliente
-        return "verEmpresas";
+        return "verEmpresas"; //llamamos el html
+        //Los que nos retornan un template, son a los que les llega el @ModelAttribute("mensaje")....
     }
 
 
     @GetMapping("/agregarEmpresa")  //Es de tipo get porque va a mostar un String (html)
-    public String createdEmpresa(Model model) {  //El metodo es string porque devuelve el nombre del template
+    public String cargarCreatedEmpresa(Model model, @ModelAttribute("mensaje")String mensaje) {  //El metodo es string porque devuelve el nombre del template
         // Crea una nueva instancia de la clase Empresa
         Empresa empr = new Empresa();
 
         // Agrega la instancia de Empresa al modelo
         model.addAttribute("emp", empr);
+        model.addAttribute("mensaje", mensaje);
 
         // Devuelve el nombre de la vista "agregarEmpresa"
         return "agregarEmpresa";
@@ -53,37 +57,48 @@ public class EmpresaController {
     @PostMapping("/guardarEmpresa") //El atributo RedirectAttributes me permite redireccionar
     public String saveEmpresa(Empresa empresa, RedirectAttributes redirectAttributes) {
         if (empresaService.saveOrUpdateEmpresa(empresa) == true) {
+            redirectAttributes.addFlashAttribute("mensaje","saveOk");
             return "redirect:/VerEmpresas";
         }
-        return "agregarEmpresa";
-        //el redirect se pone si necesito que me lleve a otro servicio, en caso tal solo pongo el tenplate o pag web
+        redirectAttributes.addFlashAttribute("mensaje","saveError");
+        return "redirect:/agregarEmpresa";
+        //el redirect se pone si necesito que me lleve a otro servicio (va a la BD), en caso tal solo pongo el tenplate o pag web
     }
 
     @GetMapping("/editarEmpresa/{id}")
-    public String editarEmpresa(Model model, @PathVariable Long id){
+    public String cargarEditarEmpresa(Model model, @PathVariable Long id, @ModelAttribute("mensaje")String mensaje){
         Empresa emp=empresaService.getEmpresaById(id);
         //Creamos un atributo para el modelo, que se llame igualmente emp y es el que ira al html para llenar o alimentar campos
         model.addAttribute("emp",emp);
+        model.addAttribute("mensaje", mensaje);
         return "editarEmpresa";
     }
 
     @PostMapping("/actualizarEmpresa")
-    public String updateEmpresa(Empresa empresa) { //tambien se podria poner asi: (@ModelAttribute("emp") Empresa empresa)
+    public String updateEmpresa(Empresa empresa, RedirectAttributes redirectAttributes) { //tambien se podria
+        // poner asi: (@ModelAttribute("emp") Empresa empresa), el otro atributo es para poner el mensaje
         if (empresaService.saveOrUpdateEmpresa(empresa)) { //le quito el == true porque lo asume por defecto
+            redirectAttributes.addFlashAttribute("mensaje","updateOk"); //Asi pongo el mensaje
             return "redirect:/VerEmpresas";
         }
-        return "editarEmpresa";
+        redirectAttributes.addFlashAttribute("mensaje","updateError");
+        return "redirect:/editarEmpresa";
     }
 
     @GetMapping("/eliminarEmpresa/{id}")  //Es un GetMapping porque consulta y quita pero no lleva nada
-    public String eliminarEmpresa(@PathVariable Long id) {
+    public String eliminarEmpresa(@PathVariable Long id, RedirectAttributes redirectAttributes) {
         try{
             empresaService.deleteEmpresaById(id);
         } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("mensaje","deleteError");
             return "redirect:/VerEmpresas";
         }
+        redirectAttributes.addFlashAttribute("mensaje","deleteOk");
         return "redirect:/VerEmpresas";
     }
+
+
+    //VOY CLASE 12 MIN 27
 
 }
 
